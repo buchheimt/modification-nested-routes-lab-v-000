b4cd670fbb858f40956eb826819757bd75543327
Tyler <tbuchhei@alumni.nd.edu>
2017-09-21 19:16:23 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -25,7 +25,11 @@ class SongsController < ApplicationController
   end
 
   def new
-    @song = Song.new
+    @song = Song.new(artist_id: params[:artist_id])
+    @artist = Artist.find_by_id(params[:artist_id])
+    if params[:artist_id] && !@artist
+      redirect_to artists_path(@artist), alert: "Artist not found"
+    end
   end
 
   def create
@@ -39,7 +43,15 @@ class SongsController < ApplicationController
   end
 
   def edit
-    @song = Song.find(params[:id])
+    @song = Song.find_by_id(params[:id])
+    @artist = Artist.find_by_id(params[:artist_id])
+    if params[:artist_id]
+      if !@artist
+        redirect_to artists_path, alert: "Artist not found"
+      elsif !@artist.songs.find_by_id(params[:id])
+        redirect_to artist_songs_path(@artist), alert: "Song not found"
+      end
+    end
   end
 
   def update
@@ -64,7 +76,6 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title, :artist_name)
+    params.require(:song).permit(:title, :artist_name, :artist_id)
   end
 end
-
--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,11 @@
 module SongsHelper
 
+  def artist_select(song, artist)
+    if artist
+      (hidden_field_tag "song[artist_id]", artist.id) + (label_tag artist.name)
+    elsif
+      val = song.artist ? song.artist.id : 1
+      select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name, val)
+    end
+  end
 end
